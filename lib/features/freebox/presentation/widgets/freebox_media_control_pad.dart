d8e6cbbd7422249ecb5f130f43823c17d9d0ca44
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-06-11 20:09:36 +0200

    [presentation] replaced freebox media control row with freebox media control pad

--- lib/features/freebox/presentation/widgets/freebox_media_control_pad.dart
+++ lib/features/freebox/presentation/widgets/freebox_media_control_pad.dart
@@ -3,16 +3,55 @@ import 'package:freebox_remote_controller/features/freebox/presentation/widgets/
 import 'package:freebox_remote_controller/features/freebox/presentation/widgets/freebox_controller_box.dart';
 import 'package:freebox_remote_controller/features/freebox/value_objects/freebox_input.dart';
 
-class FreeboxMediaControlRow extends StatelessWidget {
+class FreeboxMediaControlPad extends StatelessWidget {
   final FreeboxInputCallback onTap;
   final FreeboxInputCallback onLongPress;
-  FreeboxMediaControlRow({
+  FreeboxMediaControlPad({
     super.key,
     required this.onTap,
     required this.onLongPress,
   });
 
   late final List<FreeboxControllerBox> controls = [
+    FreeboxButtonWidget.icon(
+      input: FreeboxInputs.volUp,
+      iconData: Icons.add,
+      onTap: onTap,
+      onLongPress: onLongPress,
+    ),
+    FreeboxButtonWidget.icon(
+      input: FreeboxInputs.mute,
+      iconData: Icons.volume_off,
+      onTap: onTap,
+      onLongPress: onLongPress,
+    ),
+    FreeboxButtonWidget.icon(
+      input: FreeboxInputs.prgmUp,
+      iconData: Icons.add,
+      onTap: onTap,
+      onLongPress: onLongPress,
+    ),
+    FreeboxControllerTextBox(text: FreeboxInputs.volPlaceholder.value),
+    const FreeboxControllerPlaceholderBox(),
+    FreeboxControllerTextBox(text: FreeboxInputs.progPlaceholder.value),
+    FreeboxButtonWidget.icon(
+      input: FreeboxInputs.volDown,
+      iconData: Icons.remove,
+      onTap: onTap,
+      onLongPress: onLongPress,
+    ),
+    FreeboxButtonWidget.icon(
+      input: FreeboxInputs.rec,
+      iconData: Icons.fiber_manual_record,
+      onTap: onTap,
+      onLongPress: onLongPress,
+    ),
+    FreeboxButtonWidget.icon(
+      input: FreeboxInputs.prgmDown,
+      iconData: Icons.remove,
+      onTap: onTap,
+      onLongPress: onLongPress,
+    ),
     FreeboxButtonWidget.icon(
       input: FreeboxInputs.backward,
       iconData: Icons.fast_rewind,
