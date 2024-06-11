0c37baf8dafd14b3986818b52f3b6b77842c6d22
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-06-11 16:35:36 +0200

    [value_objects] freebox code

--- lib/features/freebox/data/data_source/freebox_api.dart
+++ lib/features/freebox/data/data_source/freebox_api.dart
@@ -1,8 +1,10 @@
+import 'package:freebox_remote_controller/features/freebox/value_objects/freebox_code.dart';
+
 import '../../value_objects/freebox_input.dart';
 
 abstract interface class FreeboxApi {
   Future<void> sendCommand({
-    required String code,
+    required FreeboxCode code,
     required FreeboxInput input,
     bool longPress = false,
   });
--- lib/features/freebox/domain/repository/freebox_repository_interface.dart
+++ lib/features/freebox/domain/repository/freebox_repository_interface.dart
@@ -1,11 +1,12 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:freebox_remote_controller/core/result/result.dart';
+import 'package:freebox_remote_controller/features/freebox/value_objects/freebox_code.dart';
 import 'package:freebox_remote_controller/features/freebox/value_objects/freebox_input.dart';
 
 abstract interface class FreeboxRepositoryInterface {
-  TaskEither<Failure, EmptySuccess> saveCode(String code);
+  TaskEither<Failure, EmptySuccess> saveCode(FreeboxCode code);
 
-  TaskEither<Failure, Success<String?>> getCode();
+  TaskEither<Failure, Success<FreeboxCode?>> getCode();
 
   TaskEither<Failure, EmptySuccess> sendCommand({
     required FreeboxInput input,
