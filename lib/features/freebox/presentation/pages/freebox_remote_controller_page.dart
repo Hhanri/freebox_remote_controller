562c345210d6360cd98e0946b17606f3d270b7c8
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-06-12 15:37:59 +0200

    freebox remote controller page removed useless sizedbox

--- lib/features/freebox/presentation/pages/freebox_remote_controller_page.dart
+++ lib/features/freebox/presentation/pages/freebox_remote_controller_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:freebox_remote_controller/core/utils/app_config.dart';
 import 'package:freebox_remote_controller/features/freebox/presentation/cubits/freebox_controller_cubit/freebox_controller_cubit.dart';
 import 'package:freebox_remote_controller/features/freebox/presentation/widgets/freebox_arrow_pad.dart';
 import 'package:freebox_remote_controller/features/freebox/presentation/widgets/freebox_home_row.dart';
@@ -18,27 +17,23 @@ class FreeboxRemoteControllerPage extends StatelessWidget {
     final onLongPress = cubit.onLongPress;
 
     return Scaffold(
-      body: SizedBox(
-        height: AppConfig.trueScreenHeight(context),
-        width: AppConfig.screenWidth(context),
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              FreeboxPowerRow(onTap: onTap, onLongPress: onLongPress),
-              const SizedBox(height: 12),
-              FreeboxNumpad(onTap: onTap, onLongPress: onLongPress),
-              const SizedBox(height: 12),
-              FreeboxArrowPad(onTap: onTap, onLongPress: onLongPress),
-              const SizedBox(height: 12),
-              FreeboxHomeRow(onTap: onTap, onLongPress: onLongPress),
-              const SizedBox(height: 12),
-              FreeboxMediaControlPad(onTap: onTap, onLongPress: onLongPress)
-            ],
-          ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FreeboxPowerRow(onTap: onTap, onLongPress: onLongPress),
+            const SizedBox(height: 12),
+            FreeboxNumpad(onTap: onTap, onLongPress: onLongPress),
+            const SizedBox(height: 12),
+            FreeboxArrowPad(onTap: onTap, onLongPress: onLongPress),
+            const SizedBox(height: 12),
+            FreeboxHomeRow(onTap: onTap, onLongPress: onLongPress),
+            const SizedBox(height: 12),
+            FreeboxMediaControlPad(onTap: onTap, onLongPress: onLongPress)
+          ],
         ),
       ),
     );
